620f8e6c45a1ea6e24a8fcaa6639aeb42d55e630
duyaya18 <yangleilei17@gmail.com>
2024-06-06 11:18:29 +0800

    方案一，这种方法使用了 TF-IDF 特征提取和朴素贝叶斯分类器，这在许多文本分类任务中都表现良好。

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import numpy as np
 import pandas as pd
-
+import tensorflow as tf
+import keras_core as keras
+import keras_nlp
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 # print("TensorFlow version:", tf.__version__)
 # print("KerasNLP version:", keras_nlp.__version__)
 
